1dd19f3e9b2a4fc1b15563c35360507e2675ebf3
Heba-Alii <hebaaliali18@gmail.com>
2023-01-13 16:06:00 +0200

    get Details Request

--- app/src/main/java/com/heba/movies/ui/details/DetailsFragment.java
+++ app/src/main/java/com/heba/movies/ui/details/DetailsFragment.java
@@ -14,8 +14,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
 import com.heba.movies.R;
 import com.heba.movies.databinding.FragmentDetailsBinding;
+import com.heba.movies.pojo.DetailsModel;
 import com.heba.movies.pojo.MoviesModel;
 
 public class DetailsFragment extends Fragment {
@@ -30,15 +32,25 @@ public class DetailsFragment extends Fragment {
         View root = binding.getRoot();
         // return inflater.inflate(R.layout.fragment_details, container, false);
         detailsViewModel = ViewModelProviders.of(this).get(DetailsViewModel.class);
-        detailsViewModel.movieListMutableLiveData.observe(this, new Observer<MoviesModel>() {
+        detailsViewModel.getMoviesDetails(1);
+        detailsViewModel.detailsModelMutableLiveData.observe(this, new Observer<DetailsModel>() {
             @Override
-            public void onChanged(MoviesModel moviesModel) {
-                binding.movieTitle.setText(moviesModel.getTitle());
-                binding.movieDetailsYearProduction.setText(moviesModel.getReleaseDate());
-                binding.movieDesc.setText(moviesModel.getOverview());
+            public void onChanged(DetailsModel detailsModel) {
+                binding.movieTitle.setText(detailsModel.getOriginal_title());
+                binding.movieDetailsYearProduction.setText(detailsModel.getRelease_date());
+                binding.movieDesc.setText(detailsModel.getOverview());
+                movieImage(detailsModel.getPoster_path());
+                binding.ratingBar.setRating(detailsModel.getVote_average());
             }
         });
         return root;
+    }
+
+    public void movieImage(String url) {
+        Glide.with(this)
+                .load(url)
+                .fitCenter()
+                .into(binding.imgDetails);
     }
 
 }
